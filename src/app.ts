f151adf7bf55020c5b1726440c5fd31b4df774e3
Nicole <niipimenta@hotmail.com>
2023-07-18 11:02:10 -0300

    feat:inicializando ambiente e express

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, json, Request, Response } from "express";
+
+const app: Application = express();
+app.use(json());
+
+app.post("/", (request: Request, response: Response): Response => {
+  return response.status(201).json(request.body);
+});
+
+app.get("/", (request: Request, response: Response): Response => {
+  return response.status(200).json({ status: "ok" });
+});
+
+const PORT: number = 3000;
+const runningMsg: string = `Server running on http://localhost:${PORT}`;
+app.listen(PORT, () => console.log(runningMsg));
